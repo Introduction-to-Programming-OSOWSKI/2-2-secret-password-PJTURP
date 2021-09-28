737f634f5c1bcc4491cd995395dda02a8fe46e90
PJTURP <821042@mystma.org>
2021-09-28 09:08:58 -0500

    Attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+#def password
+def password(x):
+    if x == "Knights19":
+        return "ACCSESS GRANTED"
+    
+    else:
+        return "ACCESS DENIED"
+
+print(password("Knights19"))
